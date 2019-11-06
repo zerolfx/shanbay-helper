996a013f7b598c01d29e291068f811caf9211b39
zerol <zerolfx0@gmail.com>
2019-11-06 22:13:04 +0800

    add: sidebar

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,7 @@
+function getHTML(e) {
+  return e.length > 0 ? e[0].outerHTML : undefined;
+}
+
 chrome.runtime.onMessage.addListener(
 function(request, sender, sendResponse) {
   const url = `https://dict.youdao.com/w/eng/${request}`;
@@ -5,7 +9,14 @@ function(request, sender, sendResponse) {
     const doc = $('<div></div>');
     doc.html(data);
     const res = {};
-    res.collins = doc.find('#collinsResult').find('.ol')[0].outerHTML;
+    res.collins = getHTML(doc.find('#collinsResult').find('.ol'));
+    res.extra = [
+      { name: "词组短语", html: getHTML(doc.find('#wordGroup')) },
+      { name: "同近义词", html: getHTML(doc.find('#synonyms')) },
+      { name: "同根词", html: getHTML(doc.find('#relWordTab')) },
+      { name: "词语辨析", html: getHTML(doc.find('#discriminate')) },
+    ];
+    console.log(res);
     sendResponse(res);
   });
   return true;
--- src/js/youdao.js
+++ src/js/youdao.js
@@ -4,7 +4,26 @@ setInterval(() => {
     ele[0].parentElement.setAttribute('id', 'collinsResult');
     const word = $("div[class^='VocabPronounce_word']")[0].innerText;
     chrome.runtime.sendMessage(word, function(response) {
-      ele.replaceWith(response.collins)
+      ele.replaceWith(response.collins);
+      const main = $('#root > div > div > div > div > div:nth-child(1) > div > div')[0];
+      main.setAttribute('class', main.className.replace("span12", "span8"));
+      main.setAttribute('style', '');
+      const side = document.createElement('div');
+      side.setAttribute('class', 'span4');
+      side.setAttribute('id', 'sidebar');
+      let sideHTML = "";
+      response.extra.forEach((x) => {
+        if (x.html) {
+          sideHTML += `
+            <div class="sidebar-block">
+              <b>${x.name}</b>
+              ${x.html}
+            </div>
+          `
+        }
+      });
+      side.innerHTML = sideHTML;
+      main.parentElement.appendChild(side);
     });
   }
 }, 500);
